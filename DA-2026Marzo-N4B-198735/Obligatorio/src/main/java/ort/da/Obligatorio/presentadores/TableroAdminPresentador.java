dd04c2aa9e16c05f6df5e6a6a23ab6dcf3e6f0b9
Laura Stirling <ls198735@fi365.ort.edu.uy>
2026-06-17 20:15:06 -0300

    SSECarrera

--- DA-2026Marzo-N4B-198735/Obligatorio/src/main/java/ort/da/Obligatorio/presentadores/TableroAdminPresentador.java
+++ DA-2026Marzo-N4B-198735/Obligatorio/src/main/java/ort/da/Obligatorio/presentadores/TableroAdminPresentador.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import jakarta.servlet.http.HttpSession;
 import ort.da.Obligatorio.dominio.Carrera;
@@ -143,7 +146,12 @@ public class TableroAdminPresentador implements Observador {
                         .map(CarreraDto.CarreraProximaDto::new)
                         .toList()));
     }
-
+    @GetMapping(value = "/registrarSSE", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+public SseEmitter registrarSSE(HttpSession session) {
+    this.session = session;
+    conexionNavegador.conectarSSE();
+    return conexionNavegador.getConexionSSE();
+}
     @Override
     public void actualizar(Observable origen, Object evento) {
         if (!(origen instanceof Carrera carrera) || !(evento instanceof Observable.Evento eventoCarrera)) {
